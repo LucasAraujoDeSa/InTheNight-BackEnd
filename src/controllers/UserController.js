ed516a809f78e1338205ef53cac3413eff6ef42c
LucasAraujo <andrelucassaaraujo@gmail.com>
2020-08-17 12:22:25 -0400

    .

---
+++ src/controllers/UserController.js
@@ -0,0 +1,40 @@
+const UserModel = require('../models/UserModel')
+
+class UserController{
+    async create(req,res){
+        try{
+            const user = await new UserModel(req.body)
+            user.save()
+            return res.status(200).json({response: 'user created'})
+        }catch(err){
+            return res.status(400).json({err: "not created"})
+        }
+    }
+    async all(req,res){
+        try{
+            const all = await UserModel.find().sort("createdAt")
+            return res.status(200).json(all)
+        }catch(err){
+            return res.status(400).json({err})
+        }        
+    }
+    async update(req,res){
+        try{
+            await UserModel.findOneAndUpdate({"_id":req.params.id},req.body,{new: true})
+            return res.status(200).json({response: "update"})
+        }catch(err){
+            console.log(err)
+            res.status(500).json({err})
+        }
+    }
+    async delete(req,res){
+        try{
+            await UserModel.findByIdAndRemove({"_id":req.params.id})
+            return res.status(200).json({response: "removed"})
+        }catch(err){
+            return res.status(400).json({err})
+        }
+    }
+}
+
+module.exports = new UserController()
